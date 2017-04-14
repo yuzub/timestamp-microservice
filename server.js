a61ea256f51a7299ad5a625d356a702197368f06
Yuriy Zubaha <yuriy.zubaha@gmail.com>
2017-04-14 10:39:12 +0000

    Modifying index.pug

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
 var express = require('express');
 var app = express();
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+  res.render('index');
+  // res.send('Hello World!');
+});
+
+app.get('/:ts', function(req, res) {
+  var ts = req.params.ts;
+  var tsDate, unixTime, naturalTime;
+
+  if (!isNaN(parseFloat(ts)) && isFinite(ts)) {
+    tsDate = new Date(+ts * 1000);
+  }
+  else {
+    tsDate = new Date(ts);
+  }
+
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  unixTime = tsDate.valueOf() / 1000;
+  if (unixTime) {
+    naturalTime = monthNames[tsDate.getUTCMonth()] + ' ' + tsDate.getUTCDate() + ', ' + tsDate.getUTCFullYear();
+  }
+  else {
+    naturalTime = null;
+  }
+
+  res.json({
+    "unix": unixTime,
+    "natural": naturalTime
+  });
 });
 
 app.listen(8080, function() {
-    console.log('Example app listening on port 8080...');
+  console.log('Example app listening on port 8080...');
 });
+
+/*The actual range of times supported by ECMAScript Date objects is slightly smaller: exactly –100,000,000 days to 100,000,000 days measured relative to midnight at the beginning of 01 January, 1970 UTC. 
+This gives a range of 8,640,000,000,000,000 milliseconds to either side of 01 January, 1970 UTC.
+
+The exact moment of midnight at the beginning of 01 January, 1970 UTC is represented by the value +0.
+The third paragraph being the most relevant. Based on that paragraph, we can get the precise earliest date per spec from new Date(-8640000000000000), 
+which is Tuesday, April 20th, 271,821 BCE (BCE = Before Common Era, e.g., the year -271,821).*/
